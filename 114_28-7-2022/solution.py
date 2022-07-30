c6256a8de02d3fc6d997c62db1aa7cab738ec01c
Samuel Roach <samuelroach.2000@gmail.com>
2022-07-30 13:47:51 +0100

    Add problem 114 solution

--- 114_28-7-2022/solution.py
+++ 114_28-7-2022/solution.py
@@ -12,4 +12,22 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        pass
+        if root == None:
+            return None
+        else:
+
+            if root.left == None and root.right == None:
+                return None
+            elif root.left != None:
+                self.flatten(root.left)
+                tmp_right = root.right
+                root.right = root.left
+                root.left = None
+
+                itr = root.right
+                while itr.right != None:
+                    itr = itr.right
+
+                itr.right = tmp_right
+
+            self.flatten(root.right)
