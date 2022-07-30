8185aa3e96e296b1b2eeb49fc4a81b87899eee9f
Samuel Roach <samuelroach.2000@gmail.com>
2022-07-30 14:58:45 +0100

    Add 916 solution

--- 916_30-7-2022/run.py
+++ 916_30-7-2022/run.py
@@ -3,7 +3,7 @@ from solution import Solution
 s = Solution()
 
 # Example 1
+print(s.wordSubsets(words1=["amazon","apple","facebook","google","leetcode"], words2=["e","o"]))
 
 # Example 2
-
-# Example 3
+print(s.wordSubsets(words1=["amazon","apple","facebook","google","leetcode"], words2=["l","e"]))
--- 916_30-7-2022/solution.py
+++ 916_30-7-2022/solution.py
@@ -1,3 +1,31 @@
+import enum
+
+
 class Solution:
     def wordSubsets(self, words1: list[str], words2: list[str]) -> list[str]:
-        
+        def letter_count(word):
+            letters = [0] * 26
+            for letter in word:
+                letters[ord(letter) - ord('a')] += 1
+            return letters
+
+        bmax = [0] * 26
+
+        for subset in words2:
+            for idx, letter in enumerate(letter_count(subset)):
+                if bmax[idx] < letter:
+                    bmax[idx] = letter
+
+        universal_words = []
+
+        for word in words1:
+            universal = True
+
+            for idx, letter in enumerate(letter_count(word)):
+                if letter < bmax[idx]:
+                    universal = False
+
+            if universal:
+                universal_words.append(word)
+
+        return universal_words
