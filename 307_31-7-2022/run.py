cf74f8b2c971b41743357f688cc4b66f5997a16e
Samuel Roach <samuelroach.2000@gmail.com>
2022-07-31 01:40:53 +0100

    Add 307 solution

--- 307_31-7-2022/run.py
+++ 307_31-7-2022/run.py
@@ -1,9 +1,7 @@
-from solution import Solution
-
-s = Solution()
+from solution import NumArray
 
 # Example 1
-
-# Example 2
-
-# Example 3
+num_arr = NumArray([1,3,5])
+print(num_arr.sumRange(0,2))
+num_arr.update(1,2)
+print(num_arr.sumRange(0,2))
--- 307_31-7-2022/solution.py
+++ 307_31-7-2022/solution.py
@@ -1,13 +1,38 @@
+from math import ceil, sqrt
+
 class NumArray:
 
-    def __init__(self, nums: List[int]):
-        pass
+    def __init__(self, nums: list[int]):
+        self.nums = nums
+
+        block_size = int(sqrt(len(nums)))
+        self.block_store_length = int(ceil(len(nums) / block_size))
+
+        self.block_store = [0] * self.block_store_length
+        for i, num in enumerate(nums):
+            self.block_store[int(i / self.block_store_length)] += num
 
     def update(self, index: int, val: int) -> None:
-        pass
+        block = int(index / self.block_store_length)
+        self.block_store[block] = self.block_store[block] - self.nums[index] + val
+        self.nums[index] = val
 
     def sumRange(self, left: int, right: int) -> int:
-        pass
+        int_sum = 0
+        start_block = int(left / self.block_store_length)
+        end_block = int(right / self.block_store_length)
+
+        if start_block == end_block:
+            int_sum += sum(self.nums[left:right + 1])
+        else:
+            # All the block vals
+            int_sum += sum(self.block_store[start_block + 1:end_block])
+            # All the beginning
+            int_sum += sum(self.nums[left:(start_block + 1) * self.block_store_length ])
+            # All the end
+            int_sum += sum(self.nums[end_block * self.block_store_length:right + 1])
+
+        return int_sum
 
 
 # Your NumArray object will be instantiated and called as such:
