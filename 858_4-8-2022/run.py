0aa8122ce9906ec46c7a18a934e2adbf534ddb9f
Samuel Roach <samuelroach.2000@gmail.com>
2022-08-04 11:06:29 +0100

    Add 858 problem and solution

---
+++ 858_4-8-2022/run.py
@@ -0,0 +1,15 @@
+from solution import Solution
+
+s = Solution()
+
+# Example 1
+print(s.mirrorReflection(2, 1))
+
+# Example 2
+print(s.mirrorReflection(3, 1))
+
+# Example 3
+print(s.mirrorReflection(3, 3))
+
+# Example 4
+print(s.mirrorReflection(4, 3))
